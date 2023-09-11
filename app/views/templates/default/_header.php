a8c353d18a8642cc423ca548d0c5c9ebfc6eb9f9
Carlo Pietrobattista <irony00100@gmail.com>
2023-09-12 03:01:33 +0700

    upgraded aw.js file

--- app/views/templates/default/_header.php
+++ app/views/templates/default/_header.php
@@ -4,6 +4,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"/>
 	<title><?=ucwords($_lang->val($_router->page() . '_page_title'))?></title>
 	<meta name="description" content="" />
+	<meta name='robots' content='noindex,follow' />
 	<?=$_resources->links()?>
 	<?=$_resources->raw('_tpl_helpers-js', 'js', 'Helpers')?>
 	<!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
